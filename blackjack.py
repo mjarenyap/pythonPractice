d7ae173acf85a89e1dd7f5d7ac333699e22c57db
Mike Jaren Yap <mjaren.yap@gmail.com>
2018-06-25 21:44:05 +0800

    Submitting Blackjack problem

---
+++ blackjack.py
@@ -0,0 +1,18 @@
+def blackjack(a, b):
+	if(a == 11):
+		a = 1
+
+	if(b == 11):
+		b = 1
+
+	result = a + b
+	if(result > 21):
+		return 0
+
+	else:
+		return result
+
+print("Type 2 numbers: ")
+a = int(input(""))
+b = int(input(""))
+print(blackjack(a, b));
